1c88312370c579c7c52e5b825a302030d15192f3
Christoph <christoph.gutsche@tu-berlin.de>
2018-02-27 12:00:21 +0100

    New size

--- src/model/model.js
+++ src/model/model.js
@@ -6,6 +6,6 @@ import { Button, Jumbotron, Panel, Grid, Row, Col, Table, Image, Thumbnail, Chec
 
 const Model = props => 
     <a className="sketchfab-embed-wrapper">
-     <iframe  width="500" height="350" src="https://sketchfab.com/models/75294282cf61466c94e36fe44db791f9/embed" frameBorder="0"></iframe>
+     <iframe  width="700" height="350" src="https://sketchfab.com/models/75294282cf61466c94e36fe44db791f9/embed" frameBorder="0"></iframe>
     </a>;
 export default Model;  
