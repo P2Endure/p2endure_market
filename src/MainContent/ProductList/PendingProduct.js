556d5e103c39ad2d3cf1af9694e1eda44fb725bb
Christoph <christoph.gutsche@tu-berlin.de>
2018-07-27 12:50:40 +0200

    Add new Product Input Form

--- src/MainContent/ProductList/PendingProduct.js
+++ src/MainContent/ProductList/PendingProduct.js
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PendingProduct = props => {
-    if(props.name){
-      return(  
-      <li className="pending">
-        <span>
-            {props.name}
-        </span>
-      </li>
-      );
-    }
-    return null;
-};
-
-PendingProduct.propTypes = {
-    name: PropTypes.string.isRequired
-};   
+    if (props.name) {
+        return (
+          <li className="pending">
+            <span>
+              {props.name}
+            </span>
+          </li>
+        );
+      }
+      return null;
+    };
+    
+    PendingProduct.propTypes = {
+      name: PropTypes.string.isRequired
+    };
 
 export default PendingProduct;
