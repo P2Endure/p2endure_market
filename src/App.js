b0d90b635043cd28194da3cc94de1048418cd36c
Christoph <christoph.gutsche@tu-berlin.de>
2018-07-27 12:49:18 +0200

    Add new Product Input Form

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './styles/css/app.css';
 import './styles/css/responsive.css';
-
+//import AddProductForm from './AddProductForm';
 //import Form from './DynamicForm/Forms/windowForm';
 import NavBar from './styles/components/NavBar.js';
-import DrawerButton from './DynamicForm/Forms/toggleDrawerButton.js'
+//import DrawerButton from './DynamicForm/Forms/toggleDrawerButton.js'
 import Header from './styles/components/index.js';
 import Sidebar from './styles/components/Sidebar.js';
 import Jumbo from './styles/components/jumbo.js';
@@ -16,7 +16,7 @@ import Footer from './styles/components/footer.js';
 import Products from './styles/components/productBar.js';
 import Model from './model/model.js';
 import Diagram from './model/diagram.js';
-
+import ProductField from './DynamicForm/Forms/select.js';
 import MainContent from './MainContent';
 
 import img from  'react';
@@ -49,21 +49,21 @@ componentDidMount(){
   this.fetchData();
 }
 
-fetchData(){
-  var json2 = require('./model/data/ProductData/Material.json')
-  for (var i in json2){
-    this.json2([i]["Material"], ["Name"])
-    console.log('IDF max field ' + JSON.stringify(json2[i]["Material"], ["Name"]))
-  }
-}
+// fetchData(){
+//   var json2 = require('./model/data/ProductData/Material.json')
+//   for (var i in json2){
+//     this.json2([i]["Material"], ["Name"])
+//     console.log('IDF max field ' + JSON.stringify(json2[i]["Material"], ["Name"]))
+//   }
+// }
 
-changeData(){
+// changeData(){
   
-    var json2 = require('./model/data/BuildingDataBevorRenovation/Arcona_BEM.json')
-    for (var i in json2){
-    console.log(JSON.stringify(json2[i]["WindowMaterial:Shade"]));
-    }
-  };
+//     var json2 = require('./model/data/BuildingDataBevorRenovation/Arcona_BEM.json')
+//     for (var i in json2){
+//     console.log(JSON.stringify(json2[i]["WindowMaterial:Shade"]));
+//     }
+//   };
 
 
 render(){
@@ -73,16 +73,18 @@ render(){
         <p>
           {JSON.stringify(this.state.fields, null, 2)}
         </p>  */}
-      <NavBar/> 
+      <NavBar/>
+      {/* <ProductField/> */}
+      {/* <AddProductForm/>  */}
       <Sidebar/>
       <Jumbo/>
-      <Products/>
-      <Button onClick ={this.changeData}>
-          {this.setState ? '1' : '2'}
-        </Button> 
-      <MuiThemeProvider>
+      {/* <MuiThemeProvider>
         <DrawerButton />
-      </MuiThemeProvider> 
+      </MuiThemeProvider> */}
+      <Products/>
+      {/* <Button onClick ={this.changeData}>
+          {this.setState ? '1' : '2'}
+        </Button>   */}
     <div className="container-fluid">
       <Row>
         <Col  md={4} lg={5}>
