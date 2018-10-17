13f5ce621f8a904093d05c3afdad0a05c8ddcaf7
Christoph <christoph.gutsche@tu-berlin.de>
2018-10-17 14:12:58 +0200

    Delete material-ui, add new parameters

--- src/MainContent/ProductList/windowForm.js
+++ src/MainContent/ProductList/windowForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Input from '@material-ui/core/Input';
-import UploadPicture from './UploadPicture.js';
 
-export default class FormWindow extends React.Component{
+export default class GlazingForm extends React.Component{
     state ={
-        U_Factor:'',
-        Solar_Heat_Gain_Coefficient:'',
-        Visible_Transmittance:''
+        Name:" ",
+        Optical_Data_Type:" ", 
+        Window_Glass_Spectral_Data_Set_Name: " ",
+        Thickness:" ",
+        Visible_Transmittance:" ",
+        Incidence:" ",
+        Conductivity:" "
     };
 
     change = e =>{
@@ -19,38 +21,69 @@ export default class FormWindow extends React.Component{
         e.preventDefault();
         this.props.onSubmit(this.state);
         this.setState({
-            U_Factor:'',
-            Solar_Heat_Gain_Coefficient:'',
-            Visible_Transmittance:''
+            Name:" ",
+            Optical_Data_Type:" ", 
+            Window_Glass_Spectral_Data_Set_Name: " ",
+            Thickness:" ",
+            Visible_Transmittance:" ",
+            Incidence:" ",
+            Conductivity:" "
         });
     };
 
     render(){
         return(
-            <form className="productForm">
-                <Input 
-                    name="U_Factor"
-                    placeholder="U-Factor" 
+            <form>
+                <input 
+                    name="Name"
+                    placeholder="Name" 
                     value={this.state.firstName} 
                     onChange={e => this.change(e)}
                 />
                 <br />
-                <Input 
-                    name="Solar_Heat_Gain_Coefficient"
-                    placeholder="Solar Heat Gain Coefficient"
+                <input 
+                    name="Optical_Data_Type"
+                    placeholder="Optical Data Type"
                     value={this.state.lastName} 
                     onChange={e => this.change(e)}
                 />
                 <br />
-                <Input 
-                    name="Visible_Transmittance"
-                    placeholder="Visible Transmittance" 
+                <input 
+                    name="Window_Glass_Spectral_Data_Set_Name"
+                    placeholder="Window Glass Spectral Data Set Name" 
                     value={this.state.username} 
                     onChange={e => this.change(e)}
                 />
                 <br />
-                <UploadPicture/>
-               <button onClick={e => this.onSubmit(e)}>Submit</button>
+                <input 
+                    name="Thickness"
+                    placeholder="Thickness" 
+                    value={this.state.firstName} 
+                    onChange={e => this.change(e)}
+                />
+                <br />
+                <input 
+                    name="Visible_Transmittance"
+                    placeholder="Visible Transmittance"
+                    value={this.state.lastName} 
+                    onChange={e => this.change(e)}
+                />
+                <br />
+                <input 
+                    name="Incidence"
+                    placeholder="Incidence" 
+                    value={this.state.username} 
+                    onChange={e => this.change(e)}
+                />
+                <br />
+                <input 
+                    name="Conductivity"
+                    placeholder="Conductivity"
+                    value={this.state.lastName} 
+                    onChange={e => this.change(e)}
+                />
+                <br />
+             
             </form>    
         )
     }
