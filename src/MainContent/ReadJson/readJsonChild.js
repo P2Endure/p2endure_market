1e548fbe4439fa36e58dae20d14820db4605024a
Christoph <christoph.gutsche@tu-berlin.de>
2018-10-18 11:15:41 +0200

    Delete

--- src/MainContent/ReadJson/readJsonChild.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class MyAppChild extends React.Component {
-    render() {
-        return <li>{this.props.Name + " - " + this.props.Roughness + " - " + this.props.Thickness}</li>;
-
-    }
-  }
