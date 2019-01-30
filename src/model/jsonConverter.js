a40143e325f8a53016e7ce781cde82ff00634f36
Christoph <christoph.gutsche@tu-berlin.de>
2019-01-30 17:29:00 +0100

    Adjust jsonConverter

--- src/model/jsonConverter.js
+++ src/model/jsonConverter.js
@@ -1,55 +1,45 @@
-
-//import React from 'react';
-
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-	
+
 const strs = [];
 const obj = {};
-const obj2 = {};
+
+keys = ['Window-Wall Ratio']
+values = ['Gross Wall Area [m2]', "Above Ground Wall Area [m2]"]
 
 JSDOM.fromFile("../upload/Warszawa_primary_validatedTable.html").then(dom => {
-	dom.window.document.querySelectorAll('b, td').forEach(str => {
+    dom.window.document.querySelectorAll('b, table').forEach(str => {
         strs.push(str.textContent)
     });
 
-keys = ['Window-Wall Ratio']
-values = ['Gross Wall Area [m2]'] 
+    strs.forEach( (tag, i) => {
+        if (keys.includes(tag)) {
+            let table = strs[i + 1];
+            let rows = table.split("\n").filter(line => line.replace(/\s/g, '').length);
 
-    strs.forEach((str, i) => {
-      if (keys.includes(str)) {
-          console.log(str)
-        obj[str] = [strs[i+1], strs[i+2], strs[i+3], strs[i+4], strs[i+5], strs[i+6], strs[i+7], strs[i+8], strs[i+9]];
-        obj[str].filter(function('Window-Wall Ratio') {
-          return keys.every(function(a) {
-            return values.includes(e[a])
-          })
-      })
-      }
-    console.log(obj)
-    console.log(obj2)
- 
-  });
+            let props = {};
 
-  
+            rows.forEach( (row, i) => {
+              values.forEach( v => {
+                  if (row.includes(v)) {
+                      let total = [];
+                      let counter = 1;
+                      while (true) {
+                          let value = parseFloat(rows[i + counter]);
+                          if (isNaN(value)) {
+                              break;
+                          } else {
+                              total.push(value);
+                              counter += 1
+                            }
 
-
-
+                        }
+                        props[v] = total; 
+                    }
+                })       
+            })
+            obj[tag] = props;
+        }
+    })
+console.log(obj)
 })
-
-
-/* const  a = ["some text"];
-  obj2[str] = obj[str].filter(word => word.length > 100)
-
-var found = array1.find(function(element) {
-	return element;
-  })
-
-const k = a[0];
-const obj = { k: [a[1], a[2], a[3]]};
-
-export default ({}) => (
- <div>
-
- </div> */
-
