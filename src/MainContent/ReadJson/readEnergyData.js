0431bf933e7c022e7223d49b709bd0b0a7dd92ec
Christoph <christoph.gutsche@tu-berlin.de>
2019-02-12 12:21:05 +0100

    Design checker

--- src/MainContent/ReadJson/readEnergyData.js
+++ src/MainContent/ReadJson/readEnergyData.js
@@ -8,7 +8,9 @@
 
 import React from 'react';
 import ReadEnergyDataChild from './readEnergyDataChild.js';
-import data from '../../model/data/ProductData/Material.json';
+import data from '../../upload/modelOutput.json';
+
+//const fs = require("fs");
 
 export default class ReadEnergyData extends React.Component {
 
@@ -18,9 +20,9 @@ export default class ReadEnergyData extends React.Component {
   }
 
   state ={
-    Solar_absorptance: '',
-    Thickness: '',
-    Thermal_absorptance: ''
+    Keyvalue: '',    
+    Materialname: '',
+    Heating: '',
 };
 
 change = e =>{
@@ -33,9 +35,10 @@ onSubmit= e =>{
     e.preventDefault();
     this.props.onSubmit(this.state);
     this.setState({
-        Solar_absorptance:key,
-        Thickness:Name,
-        Thermal_absorptance:Roughness
+
+        Keyvalue:key,
+        Materialname: Materialname,
+        Heating: Heating
     });
 };
 
@@ -46,26 +49,34 @@ onSubmit= e =>{
       alert(this.json2);
   }
 
+  onChange(){
+    var fs = require('fs'); 
+    var json = JSON.stringify(obj)   
+    fs.writeFile('../../upload/modelOutput_1.json', json); 
+  }
+
   render() {
       const fields = this.props.fields;
-      var json2 = data[0]      
+      var json2 = data[0]     
       var arr = [];
+      var obj = {};
       Object.keys(json2).forEach(function(key) {
         arr.push(json2[key]);
       });
     let children = [];
     arr.map((energyValues, i) => {
-        children.push(<ReadEnergyDataChild  key={energyValues.Name} 
-                                            Name={energyValues.Name} 
-                                            Roughness={energyValues.Roughness} 
-                                            Thickness={energyValues.Thickness} />);
+        children.push(<ReadEnergyDataChild  key={energyValues.Keyvalue} 
+                                            Name={energyValues.Keyvalue} 
+                                            Materialname={energyValues.Materialname} 
+                                            Heating={energyValues.Heating} />);
+            obj[arr]= children;
     });
     
 return(
     <div>
         <ul>
-        {children}
-        </ul>                
+            <Button onChange={this.onChange()}>Anzeigen</Button>
+        </ul>
     </div>
     )
 }
--- src/MainContent/ReadJson/readEnergyDataChild.js
+++ src/MainContent/ReadJson/readEnergyDataChild.js
@@ -7,7 +7,7 @@ import React from 'react';
 
 export default class ReadEnergyDataChild extends React.Component {
     render() {
-        return <li>{this.props.Name + " - " + this.props.Roughness + " - " + this.props.Thickness}</li>;
+        return <li>{this.props.Keyvalue + " - " + this.props.Materialname + " - " + this.props.Heating}</li>;
 
     }
   }
