bfb6939e3b9ee7fbf6ba85874744035e62397916
christoph.gutsche <christoph.gutsche@tu-berlin.de>
2019-03-12 18:06:57 +0100

    IDF converter

--- src/model/idfConverterAfter.js
+++ src/model/idfConverterAfter.js
@@ -1,44 +1,61 @@
 //idfConverterAfter,js with the toRemoveAfter value, which I want to export
 const fs = require('fs');
-
 const key = /!\s+(\=+\s+?GAP - MATERIAL GLASS\s+=+)\s+!/g;
 const end = /!/g;
+const text = '../upload/Warszawa_primary_validated.idf'
 
-values = ['Material:NoMass'];
-module.exports = {
- text : fs.readFile('../upload/Warszawa_phase2_bgtec_summermode.expidf', function(err, text) {
-    if (err) {
-        throw err;
-    }
-    /* console.log("Material:NoMass: ", getValueByKey(text, "Material:NoMass"));  */
-    const idf = text.toString().split("\n");
-    var lines_n = idf.length;
-    let block = {};
+values = ["GAP - MATERIAL GLASS", "GAP - MATERIAL FRAME", "GAP - CONSTRUCION"];
+
+module.exports = function getDataText(){
+   let data = fs.readFileSync(text, 'utf8')
+            const idf = data.toString().split("\n");
+            var lines_n = idf.length;
+            let block = {};
+    values.forEach(v => { 
+        block[v] = {};
+    })
     for (let i = 0; i <= lines_n; i++) {
         let line = idf[i];
         if (line) {
-            let isMatch = testBegin(line, "GAP - MATERIAL GLASS");
-
+            values.forEach( (v) => {
+            let isMatch = testBegin(line, v);
             if (isMatch) {
                 //console.log("match ", i)
-                block.begin = i;
+                block[v].begin = i;
                 let counter = i + 1
                 while (true) {
                     let endLine = idf[counter];
                     if (testEnd(endLine)) {
-                        block.end = counter;
+                        block[v].end = counter;
                         break;
                     }
                     counter++
+                    }   
                 }
-            }
+            })
         }
-    };
-    var toRemoveAfter = idf.splice(block.begin, block.end - block.begin); // an array of lines to remove/replace, it has been removed from idf
-    toRemoveAfter = toRemoveAfter.toString().replace(/(,\r\n|\n|\r\,)/gm,"\n");  
-    //console.log(toRemoveAfter);
-})};
+    }
+    for (var key in block){
+        if(block.hasOwnProperty(key)) {
+            let b = block[key]
+                var toRemoveAfter = idf.splice(b.begin, 94, b.end - b.begin);
+                toRemoveAfter = toRemoveAfter.toString().replace(/(,\r\n|\n|\r\,)/gm,"\n");
+                return toRemoveAfter;
+                //console.log(toRemoveAfter);
+            
+        }}
+    //let toRemoveOriginal = idf.splice(block.begin, block.end - block.begin); // an array of lines to remove/replace, it has been removed from idf
+    //toRemoveOriginal = toRemoveOriginal.toString().replace(/(\r\n|\n|\r)/gm,"\n");
+    //idf.push(toRemoveAfter);
+    //idf = idf.toString().replace(/(,\r\n|\n|\r\,)/gm,"\n");
+    //console.log(toRemoveOriginal);
+/*      var fs = require('fs');
+    fs.writeFile('../upload/modelOutput_test_3.idf', idf, 'utf8', function(e){
+        console.log("done"); 
+    }
+    );  */
 
+}
 
 function testBegin(text, key) {
     var regex = new RegExp(`(^!).*${key}`);
@@ -47,7 +64,6 @@ function testBegin(text, key) {
     } else {
         return false;
     }
-    //^!\W*((?i)GAP - MATERIAL GLASS(?-i))\W*
 }
 
 function testEnd(text) {
@@ -57,9 +73,5 @@ function testEnd(text) {
     } else {
         return false;
     }
-    //^!\W*((?i)GAP - MATERIAL GLASS(?-i))\W*
 }
-
-
-
 
--- src/model/idfConverterOriginal.js
+++ src/model/idfConverterOriginal.js
@@ -1,54 +1,62 @@
 //idfConverterBefore,js where I want to import the toRemoveAfter value
 
-const test = require('./idfConverterAfter.js')
-//console.log(JSON.stringify(test));
-
+const getDataText = require('./idfConverterAfter.js')
+const string = getDataText();
 const fs = require('fs');
-
 const key = /!\s+(\=+\s+?GAP - MATERIAL GLASS\s+=+)\s+!/g;
 const end = /!/g;
   
-values = ['Material:NoMass'];
+values = ["GAP - MATERIAL GLASS", "GAP - MATERIAL FRAME", "GAP - CONSTRUCION"];
 
 var text = fs.readFile('../upload/Warszawa_primary_validated.idf', function(err, text) {
     if (err) {
         throw err;
     }
-    /* console.log("Material:NoMass: ", getValueByKey(text, "Material:NoMass"));  */
     let idf = text.toString().split("\n");
     var lines_n = idf.length;
     let block = {};
+    values.forEach(v => { 
+        block[v] = {};
+    })
     for (let i = 0; i <= lines_n; i++) {
         let line = idf[i];
         if (line) {
-            let isMatch = testBegin(line, "GAP - MATERIAL GLASS");
-
+            values.forEach( (v) => {
+            let isMatch = testBegin(line, v);
             if (isMatch) {
                 //console.log("match ", i)
-                block.begin = i;
+                block[v].begin = i;
                 let counter = i + 1
                 while (true) {
                     let endLine = idf[counter];
                     if (testEnd(endLine)) {
-                        block.end = counter;
+                        block[v].end = counter;
                         break;
                     }
                     counter++
+                    }   
                 }
-            }
+            })
         }
-    };
-    let toRemoveOriginal = idf.splice(block.begin, block.end - block.begin); // an array of lines to remove/replace, it has been removed from idf
+    }
+    for (var key in block){
+        if(block.hasOwnProperty(key)) {
+            let b = block[key]
+                var toRemoveOriginal = idf.splice(b.begin, 94, b.end - b.begin);
+                toRemoveOriginal = toRemoveOriginal.toString().replace(/(,\r\n|\n|\r\,)/gm,"\n");
+                //console.log(toRemoveAfter);
+            
+        }}
+    //let toRemoveOriginal = idf.splice(block.begin, block.end - block.begin); // an array of lines to remove/replace, it has been removed from idf
     //toRemoveOriginal = toRemoveOriginal.toString().replace(/(\r\n|\n|\r)/gm,"\n");
-    idf.push(test);
+    idf.push(string);
     idf = idf.toString().replace(/(,\r\n|\n|\r\,)/gm,"\n");
-    //console.log(idf);
-/*     idf = idf.toString().replace(/(\r\n|\n|\r)/gm,"");
-    console.log(idf);
-    //toRemoveOriginal = toRemoveOriginal.toString().replace(/(\r\n|\n|\r)/gm,""); */
-   
     var fs = require('fs');
-    fs.writeFile('../upload/modelOutput_test_3.idf', idf, 'utf8');
+    fs.writeFile('../upload/modelOutput_test_3.idf', idf, 'utf8', function(e){
+        console.log("done"); 
+    }
+    ); 
+
 });
 
 function testBegin(text, key) {
