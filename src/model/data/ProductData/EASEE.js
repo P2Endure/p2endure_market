652d430984f9a9a64dfdf956e13cef149fe20373
Christoph Gutsche <christoph.gutsche@tu-berlin.de>
2020-02-21 11:44:17 +0100

    UpdateDataTypesProducts

--- src/model/data/ProductData/EASEE.js
+++ src/model/data/ProductData/EASEE.js
@@ -1,19 +1,45 @@
 const surface =  `
         Material,
-        EASEE_1.4cm (150mm 0.6), 	    ! Name
+        TRC_Layer, 	              ! Name
         Rough,           			    ! Roughness (*)
-        0.14,            			    ! Thickness {m}
-        0.2,           			        ! Conductivity {W/m-K}
-        40,              			    ! Density {kg/m3}
+        0.014,            			  ! Thickness {m}
+        2,           			        ! Conductivity {W/m-K}
+        2400,              			  ! Density {kg/m3}
         1000,            			    ! Specific Heat {J/kg-K}
-        0.9,             			    ! Thermal Absorptance (*)
-        0.6,             			    ! Solar Absorptance
-        0.6;             			    ! Visible Absorptance (*)`
+        0.9,                      !- Thermal Absorptance
+        0.75,                     !- Solar Absorptance
+        0.75;                     !- Visible Absorptance
+        
+        Material,
+        EPS_250, 	               ! Name
+        Rough,           			   ! Roughness (*)
+        0.01,            			   ! Thickness {m}
+        0.034,           			   ! Conductivity {W/m-K}
+        28,              			   ! Density {kg/m3}
+        1400,            			   ! Specific Heat {J/kg-K}
+        0.9,                     !- Thermal Absorptance
+        0.75,                    !- Solar Absorptance
+        0.75;                    !- Visible Absorptance
+
+        Material,
+        TRC_Layer, 	              ! Name
+        Rough,           			    ! Roughness (*)
+        0.014,            			  ! Thickness {m}
+        2,           			        ! Conductivity {W/m-K}
+        2400,              			  ! Density {kg/m3}
+        1000,            			    ! Specific Heat {J/kg-K}
+        0.9,                      !- Thermal Absorptance
+        0.75,                     !- Solar Absorptance
+        0.75;                     !- Visible Absorptance
+        `
 
 const wallConstruction = `
+        ! Name
+        ! Outside Layer
         Construction,
-        C109_ciana_podstawowa_Ribbed,   ! Name
-        M03_EASEE_1.4cm (150mm 0.6),    ! Outside Layer
-        M13_Reference (320mm 0.6);
-`        
+        C09_Basic_Wall_Foam_concrete,             !- Name
+        TRC_layer,                                !- Outside Layer
+        EPS_250,                                  !- Layer 2
+        TRC_Layer,                                !- Layer 3
+        `        
 export default Easee  
